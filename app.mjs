a4ebf2517e40bb004e5e6164fff869dcfdea0d06
kowalsking <kovvalsking@gmail.com>
2022-06-12 23:45:16 +0300

    export all

--- app.mjs
+++ app.mjs
@@ -1,5 +1,5 @@
-import { characters, greet } from './characters.mjs'
+import * as char from './characters.mjs'
 
-for (const c of characters) {
-  greet(c)
+for (const c of char.characters) {
+  char.greet(c)
 }
