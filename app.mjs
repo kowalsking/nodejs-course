e18dcf2cf9b5b11232a44c3a87e806724fdb7607
kowalsking <kovvalsking@gmail.com>
2022-06-12 23:50:54 +0300

    Use async Import as Promise

--- app.mjs
+++ app.mjs
@@ -1,11 +1,14 @@
 // import log, { characters, greet as hello } from './characters.mjs'
 
 async function main() {
-  const { characters, greet } = await import('./characters.mjs')
-  for (const c of characters) {
-    greet(c)
+  try { 
+    const { characters, greet } = await import('./characters.mjs')
+    for (const c of characters) {
+      greet(c)
+    }
+  } catch (e) {
+    console.log('Error')
   }
-  
 }
 
 main()
