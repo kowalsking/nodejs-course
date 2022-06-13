6b9cf4e44e0af9f21b870dee187317d2192562d4
kowalsking <kovvalsking@gmail.com>
2022-06-13 23:08:02 +0300

    Args

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-const start = performance.now();
+// const start = performance.now();
 
-setTimeout(() => {
-  console.log(performance.now() - start)
-}, 1000)
+// setTimeout(() => {
+//   console.log(performance.now() - start)
+// }, 1000)
+
+function myFunc(arg) {
+  console.log('Arg', arg)
+}
+
+setTimeout(myFunc, 1500, 'Cool')
