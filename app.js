2d6e7f9e1b543f94f67f82da66281998a14a2933
kowalsking <kovvalsking@gmail.com>
2022-06-13 23:29:01 +0300

    EL logs

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const fs = require('fs')
+
 console.log('Init')
 
 setTimeout(() => {
@@ -7,5 +9,22 @@ setTimeout(() => {
 setImmediate(() => {
   console.log('Immediate')
 })
+
+fs.readFile(__filename, () => {
+  console.log('File readed')
+})
+
+setTimeout(() => {
+  for (let i = 0; i < 1000000000; i++) {
+
+  }
+  console.log('DOne!')
+}, 0)
+
+Promise.resolve().then(() => {
+  console.log('Promise')
+})
+
+process.nextTick(() => console.log('tick'))
 
 console.log('Finish')
