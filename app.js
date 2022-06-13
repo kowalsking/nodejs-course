0a8f5fe7112f2d3c0bf629dc0712ab163e5e2732
kowalsking <kovvalsking@gmail.com>
2022-06-13 23:06:58 +0300

    Timers

--- app.js
+++ app.js
@@ -1,50 +1,5 @@
-const EventEmitter = require('events')
+const start = performance.now();
 
-const myEmitter = new EventEmitter()
-
-const logDbConnection = () => {
-  console.log('DB connected')
-}
-
-myEmitter.addListener('connected', logDbConnection)
-
-myEmitter.emit('connected')
-
-// myEmitter.removeListener('connected', logDbConnection)
-// myEmitter.off('connected', logDbConnection)
-// myEmitter.removeAllListener('connected')
-
-myEmitter.emit('connected')
-
-myEmitter.on('msg', data => {
-  console.log('Taked:', data)
-})
-
-myEmitter.emit('msg', 'Hello world!')
-
-myEmitter.once('off', () => {
-  console.log('1123123')
-})
-
-myEmitter.emit('off')
-myEmitter.emit('off')
-
-console.log(myEmitter.getMaxListeners())
-myEmitter.setMaxListeners(1)
-console.log(myEmitter.getMaxListeners())
-
-console.log(myEmitter.listenerCount('msg'))
-console.log(myEmitter.listenerCount('off'))
-console.log(myEmitter.eventNames())
-
-myEmitter.on('error', err => {
-  console.log(`Error: ${err.message}`)
-})
-
-myEmitter.emit('error', new Error('EEEERRRROOOORRRR'))
-
-const target = new EventTarget()
-const logTarget = () => console.log('Connected to target')
-
-target.addEventListener('connected', logTarget)
-target.dispatchEvent(new Event('connected'))
+setTimeout(() => {
+  console.log(performance.now() - start)
+}, 1000)
