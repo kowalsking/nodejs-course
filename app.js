9a4604873202c905420e99486d73d65f030cf1cc
kowalsking <kovvalsking@gmail.com>
2022-06-13 23:18:37 +0300

    Event Loop stages

--- app.js
+++ app.js
@@ -1,37 +1,11 @@
-// const start = performance.now();
+console.log('Init')
 
-// setTimeout(() => {
-//   console.log(performance.now() - start)
-// }, 1000)
+setTimeout(() => {
+  console.log(performance.now(), 'Timer o')
+}, 10)
 
-// function myFunc(arg) {
-//   console.log('Arg', arg)
-// }
+setImmediate(() => {
+  console.log('Immediate')
+})
 
-// setTimeout(myFunc, 1500, 'Cool')
-
-
-// const id = setTimeout(() => {
-//   console.log('BOOM!')
-// }, 5000)
-
-// setTimeout(() => {
-//   clearTimeout(id)
-//   console.log('Finished!')
-// }, 1000)
-
-// console.log('Before')
-
-// setImmediate(() => console.log('After all'))
-
-// console.log('After')
-
-// const id = setTimeout(() => {
-//   console.log('BOOM!')
-// }, 5000)
-
-// id.unref()
-
-// setImmediate(() =>{
-//   id.ref()
-// })
+console.log('Finish')
