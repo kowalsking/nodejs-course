cd3d43a3a9c8795f1bdd58289c644a27acfc743f
kowalsking <kovvalsking@gmail.com>
2022-06-14 10:37:54 +0300

    Call Stack

--- app.js
+++ app.js
@@ -1,30 +1,13 @@
-const fs = require('fs')
+const a = 5
 
-console.log('Init')
+function b() {
+  return c()
+}
+function c() {
+  return d()
+}
+function d() {
+  console.log(a)
+}
 
-setTimeout(() => {
-  console.log(performance.now(), 'Timer o')
-}, 10)
-
-setImmediate(() => {
-  console.log('Immediate')
-})
-
-fs.readFile(__filename, () => {
-  console.log('File readed')
-})
-
-setTimeout(() => {
-  for (let i = 0; i < 1000000000; i++) {
-
-  }
-  console.log('DOne!')
-}, 0)
-
-Promise.resolve().then(() => {
-  console.log('Promise')
-})
-
-process.nextTick(() => console.log('tick'))
-
-console.log('Finish')
+b()
