8da77824fe3ccbdeef2a9bd7f29de8374c369cb6
kowalsking <kovvalsking@gmail.com>
2022-06-14 17:35:36 +0300

    Worker threads is awesome!!!

---
+++ app-workers.js
@@ -0,0 +1,45 @@
+const factorial = require('./factorial')
+const { Worker } = require('worker_threads')
+
+const compute = (array) => {
+  return new Promise((resolve, reject) => {
+    const worker = new Worker('./worker.js', {
+      workerData: { array }
+    })
+
+    worker.on('message', msg => {
+      console.log(worker.threadId)
+      resolve(msg)
+    })
+
+    worker.on('error', err => {
+      reject(err)
+    })
+
+    worker.on('exit', () => {
+      console.log('EXIT')
+    })
+  })
+}
+
+async function main() {
+  try {
+    performance.mark('start')
+    const result = await Promise.all([
+      compute([25, 20, 19, 48, 30, 50]),
+      compute([25, 20, 19, 48, 30, 50]),
+      compute([25, 20, 19, 48, 30, 50]),
+      compute([25, 20, 19, 48, 30, 50])
+    ])
+  
+    console.log(result)
+  
+    performance.mark('end')
+    performance.measure('main', 'start', 'end')
+    console.log(performance.getEntriesByName('main').pop())
+  } catch(e) {
+    console.error(e.message)
+  }
+}
+
+main()
--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const factorial = require('./factorial')
 
 const compute = (array) => {
   const arr = []
-  for (let i = 0; i < 10000000; i++) {
+  for (let i = 0; i < 100000000; i++) {
     arr.push(i*i)
   }
   return array.map(el => factorial(el))
