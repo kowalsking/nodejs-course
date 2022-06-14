3b7ce791fd2d560b8b64a1dc58f466b8bd56e15c
kowalsking <kovvalsking@gmail.com>
2022-06-14 17:20:54 +0300

    Worker threads start

--- app.js
+++ app.js
@@ -1,33 +1,27 @@
-const perf_hooks = require('perf_hooks');
+const factorial = require('./factorial')
 
-test = perf_hooks.performance.timerify(test)
-
-const performanceObserver = new perf_hooks.PerformanceObserver((items, observer)  => {
-  console.log(items.getEntries())
-  const entry = items.getEntriesByName('slow').pop();
-  console.log(entry.name, entry.duration)
-  observer.disconnect()
-})
-
-performanceObserver.observe({ entryTypes: ['measure', 'function'] })
-
-
-function test() {
+const compute = (array) => {
   const arr = []
-  for(let i = 0; i < 10000000; i++) {
+  for (let i = 0; i < 10000000; i++) {
     arr.push(i*i)
   }
+  return array.map(el => factorial(el))
 }
 
-function slow() {
+function main() {
   performance.mark('start')
-  const arr = []
-  for(let i = 0; i < 10000000; i++) {
-    arr.push(i*i)
-  }
+  const result = [
+    compute([25, 20, 19, 48, 30, 50]),
+    compute([25, 20, 19, 48, 30, 50]),
+    compute([25, 20, 19, 48, 30, 50]),
+    compute([25, 20, 19, 48, 30, 50])
+  ]
+
+  console.log(result)
+
   performance.mark('end')
-  performance.measure('slow', 'start', 'end')
+  performance.measure('main', 'start', 'end')
+  console.log(performance.getEntriesByName('main').pop())
 }
 
-slow()
-test()
+main()
