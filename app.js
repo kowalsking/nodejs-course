d830516c63840c5863c9e2a90981aafb80baaa63
kowalsking <kovvalsking@gmail.com>
2022-06-14 10:52:53 +0300

    Another example call

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-const crypto = require('crypto')
+const https = require('https')
 const start = performance.now()
 
-process.env.UV_THREADPOOL_SIZE = 8;
-
 for (let i = 0; i < 50; i++) {
-  crypto.pbkdf2('test', 'salt', 100000, 64, 'sha512', () => {
-    console.log(performance.now() - start)
+  https.get('https://amazon.com', (res) => {
+    res.on('data', () => {})
+    res.on('end', () => {
+      console.log(performance.now() - start)
+    })
   })
 }
