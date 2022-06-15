e7bc6cc0f3949e2b06249fbc221d7ee692521521
kowalsking <kovvalsking@gmail.com>
2022-06-15 17:12:57 +0300

    Exec

--- app.js
+++ app.js
@@ -1,27 +1,12 @@
-const factorial = require('./factorial')
+const { exec } = require('child_process')
 
-const compute = (array) => {
-  const arr = []
-  for (let i = 0; i < 100000000; i++) {
-    arr.push(i*i)
-  }
-  return array.map(el => factorial(el))
-}
+const childProcess = exec('dir', (err, stdout, stderr) => {
+  if (err) console.log(err.message)
 
-function main() {
-  performance.mark('start')
-  const result = [
-    compute([25, 20, 19, 48, 30, 50]),
-    compute([25, 20, 19, 48, 30, 50]),
-    compute([25, 20, 19, 48, 30, 50]),
-    compute([25, 20, 19, 48, 30, 50])
-  ]
+  console.log(`stdout: ${stdout}`)
+  console.log(`stderr: ${stderr}`)
+})
 
-  console.log(result)
-
-  performance.mark('end')
-  performance.measure('main', 'start', 'end')
-  console.log(performance.getEntriesByName('main').pop())
-}
-
-main()
+childProcess.on('exit', (code) => {
+  console.log('exit code', code)
+})
