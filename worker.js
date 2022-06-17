1053faba8a6af87c96fe2a80f85abfc8612fb98d
kowalsking <kovvalsking@gmail.com>
2022-06-17 23:33:59 +0300

    Clear folder & create package.json

--- worker.js
+++
@@ -1,5 +0,0 @@
-const { parentPort, workerData } = require('worker_threads');
-const { compute } = require('./factorial');
-
-console.log('WorkerData', workerData);
-parentPort.postMessage(compute(workerData))
