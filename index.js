b791261d4d6616a6d207bf0eb9c847ccdbd9ad80
kowalsking <kovvalsking@gmail.com>
2022-06-19 00:02:05 +0300

    1st Express server

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from 'express'
+
+const port = 8080
+const app = express()
+
+app.get('/hello', (req, res) => {
+  res.send('Hello!')
+})
+
+app.listen(port, () => {
+  console.log('Server listen:', port)
+})
