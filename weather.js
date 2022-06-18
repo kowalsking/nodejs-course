52136392f97b33f5729fc35f72c35ed334dff1f8
kowalsking <kovvalsking@gmail.com>
2022-06-18 12:44:25 +0300

    Work with file system

--- weather.js
+++ weather.js
@@ -2,7 +2,19 @@
 
 import { getArgs } from './helpers/args.js'
 import { printHelp } from './services/log.service.js'
-import { saveKeyValue } from './services/storage.service.js'
+import { saveKeyValue, printSuccess, printError } from './services/storage.service.js'
+
+const saveToken = async token => {
+  if (!token.length) {
+    printError('No token!')
+  }
+  try {
+    await saveKeyValue('token', token)
+    printSuccess('Token saved')
+  } catch (e) {
+    printError(e.message)
+  }
+}
 
 const initCLI = () => {
   const args = getArgs(process.argv)
@@ -15,7 +27,7 @@ const initCLI = () => {
   }
 
   if (args.t) {
-    saveKeyValue('token', args.t)
+    return saveToken(args.t)
   }
 
   // show weather
