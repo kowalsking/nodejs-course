bbeda1208b36fd23327d19e32f48dacdf243682f
kowalsking <kovvalsking@gmail.com>
2022-06-18 13:06:25 +0300

    Fetch data with axios

--- services/api.service.js
+++ services/api.service.js
@@ -1,4 +1,4 @@
-import https from 'https'
+import axios from 'axios'
 import { getKeyValue, TOKEN_DICTIONARY } from './storage.service.js'
 
 const getWeather = async city => {
@@ -7,21 +7,15 @@ const getWeather = async city => {
     throw new Error('Toke didnt found!')
   }
 
-  const url = new URL('https://api.openweathermap.org/data/2.5/weather')
-  url.searchParams.append('q', city)
-  url.searchParams.append('appid', token)
-  url.searchParams.append('lang', 'en')
-  url.searchParams.append('utils', 'metric')
-  https.get(url, response => {
-    let res = ''
-    response.on('data', (chunk) => {
-      res += chunk
-    })
-
-    response.on('end', () => {
-      console.log(res)
-    })
+  const { data } = await axios.get('https://api.openweathermap.org/data/2.5/weather', {
+    params: {
+      q: city,
+      appid: token,
+      lang: 'en',
+      utils: 'metric'
+    }
   })
+  return data
 }
 
 export { getWeather }
