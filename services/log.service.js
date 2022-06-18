759b7232195f00abfe251375e1e65077bf5deafe
kowalsking <kovvalsking@gmail.com>
2022-06-18 23:07:48 +0300

    chalk bgYellow

--- services/log.service.js
+++ services/log.service.js
@@ -19,12 +19,12 @@ const printHelp = () => {
   )
 }
 
-const printWeatherData = (data) => {
-  return console.log(dedent`
+const printWeather = (data) => {
+  return console.log(dedent`${chalk.bgYellow(' W E A T H E R ')}
     Soo.. temperature: ${data.main.temp} but feels like ${data.main.feels_like}!
     Outside we have ${data.weather[0].main.toLowerCase()}, ${data.weather[0].description} to be honest...
     Нехай проблеми та невзгоди не роблять вам в житті погоди!
   `)
 }
 
-export { printError, printSuccess, printHelp, printWeatherData }
+export { printError, printSuccess, printHelp, printWeather as printWeatherData }
