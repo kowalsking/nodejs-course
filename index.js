c2af4cd017064f257e6768b049112907bd77738c
kowalsking <kovvalsking@gmail.com>
2022-06-19 23:23:22 +0300

    Error listener

--- index.js
+++ index.js
@@ -4,12 +4,21 @@ import { userRouter } from './users/users.js'
 const port = 8080
 const app = express()
 
+app.use((req, res, next) => {
+  console.log('time', Date.now())
+  next()
+})
+
 app.get('/hello', (req, res, next) => {
-  res.set('Content-Type', 'text/plain')
-  res.send('Hello!')
+  throw new Error('Error!')
 })
 
 app.use('/users', userRouter)
+
+app.use((err, req, res, next) => {
+  console.log(err.message)
+  res.status(500).send(err.message)
+})
 
 app.listen(port, () => {
   console.log('Server listen:', port)
--- users/users.js
+++ users/users.js
@@ -2,6 +2,12 @@ import express from 'express'
 
 const userRouter = express.Router()
 
+userRouter.use((req, res, next) => {
+  console.log('users router', Date.now())
+  next()
+})
+
+
 userRouter.get('/login', (req, res) => {
   res.send('login')
 })
