9a17abc169929bc5250d77d1f732edddbcde6803
kowalsking <kovvalsking@gmail.com>
2022-06-19 23:06:50 +0300

    Router

--- index.js
+++ index.js
@@ -1,21 +1,15 @@
 import express from 'express'
+import { userRouter } from './users/users'
 
 const port = 8080
 const app = express()
 
 app.all('/hello', (req, res, next) => {
-  console.log('all')
-  next()
-})
-
-const cb = (req, res, next) => {
-  console.log('cb')
-  next()
-}
-
-app.get('/hello', cb, (req, res) => {
+  res.set('Content-Type', 'text/plain')
   res.send('Hello!')
 })
+
+app.use('/users', userRouter)
 
 app.listen(port, () => {
   console.log('Server listen:', port)
