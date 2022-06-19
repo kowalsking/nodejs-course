0b22a10dc62b323f49e74d707a1e90ff4deb7859
kowalsking <kovvalsking@gmail.com>
2022-06-19 22:48:06 +0300

    App.all

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ import express from 'express'
 const port = 8080
 const app = express()
 
+app.all('/hello', (req, res, next) => {
+  console.log('all')
+  next()
+})
+
 app.get('/hello', (req, res) => {
   res.send('Hello!')
 })
