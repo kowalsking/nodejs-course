715ac6254f8e422a4c9d4ef0cf31fca99adef9d9
kowalsking <kovvalsking@gmail.com>
2022-06-22 00:04:14 +0300

    Error handler. Exception filter

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Express } from 'express'
 import { Server } from 'http'
+import { ExceptionFilter } from './errors/exception.filter'
 import { LoggerService } from './logger/logger.service'
 import { UserController } from './users/users.controller'
 
@@ -9,20 +10,31 @@ export class App {
   server: Server
   logger: LoggerService
   userController: UserController
+  exceptionFilter: ExceptionFilter
 
-  constructor(logger: LoggerService, userController: UserController) {
+  constructor(
+      logger: LoggerService,
+      userController: UserController,
+      exceptionFilter: ExceptionFilter
+    ) {
     this.app = express()
     this.port = 8080
     this.userController = userController
     this.logger = logger
+    this.exceptionFilter = exceptionFilter
   }
 
   useRoutes() {
     this.app.use('/users', this.userController.router)
   }
 
+  useExceptionFilters() {
+    this.app.use(this.exceptionFilter.catch.bind(this.exceptionFilter))
+  }
+
   public async init() {
     this.useRoutes()
+    this.useExceptionFilters()
     this.server = this.app.listen(this.port)
 
     this.logger.log('Server listening on port ' + this.port)
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,7 @@
 import { Response, Request, NextFunction } from 'express';
 import { BaseController } from '../common/base.controller';
 import { IControllerRoute } from '../common/route.interface';
+import { HTTPError } from '../errors/http-error.class';
 import { LoggerService } from '../logger/logger.service';
 
 export class UserController extends BaseController {
@@ -16,7 +17,7 @@ export class UserController extends BaseController {
   }
 
   login(req: Request, res: Response, next: NextFunction) {
-    this.ok(res, 'login')
+    next(new HTTPError(401, 'Not registered!', 'login'))
   }
 
   register(req: Request, res: Response, next: NextFunction) {
