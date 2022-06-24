64b0f510886e5f62c516bdaede18471d5b93ae31
kowalsking <kovvalsking@gmail.com>
2022-06-25 00:32:52 +0300

    User Service

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,10 @@ import { IExceptionFilter } from './errors/exception.filter.iterface'
 import { ILogger } from './logger/logger.interface'
 import { LoggerService } from './logger/logger.service'
 import { TYPES } from './types'
+import { UserService } from './users/user.service'
+import { IUserService } from './users/user.service.interface'
 import { UserController } from './users/users.controller'
+import { IUserController } from './users/users.controller.interface'
 
 export interface IBootstrapReturn {
 	appContainer: Container
@@ -15,7 +18,8 @@ export interface IBootstrapReturn {
 export const appBindings = new ContainerModule((bind: interfaces.Bind) => {
 	bind<ILogger>(TYPES.ILogger).to(LoggerService)
 	bind<IExceptionFilter>(TYPES.ExceptionFilter).to(ExceptionFilter)
-	bind<UserController>(TYPES.UserController).to(UserController)
+	bind<IUserController>(TYPES.UserController).to(UserController)
+	bind<IUserService>(TYPES.UserService).to(UserService)
 	bind<App>(TYPES.Application).to(App)
 })
 
