623325ddf6b5fe5c8b08d5b46fbb5b39517c954d
kowalsking <kovvalsking@gmail.com>
2022-06-25 11:41:08 +0300

    Middleware Validation

--- src/users/dto/user-register.dto.ts
+++ src/users/dto/user-register.dto.ts
@@ -1,5 +1,12 @@
+import { IsEmail, IsString } from 'class-validator'
+
 export class UserRegisterDto {
+	@IsEmail({}, { message: 'Bad email!' })
 	email: string
+
+	@IsString({ message: 'No Password!' })
 	password: string
+
+	@IsString({ message: 'No Name!' })
 	name: string
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -11,6 +11,7 @@ import { UserLoginDto } from './dto/user-login.dto'
 import { UserRegisterDto } from './dto/user-register.dto'
 import { User } from './user.entity'
 import { UserService } from './user.service'
+import { ValidateMiddleware } from '../common/validate.middleware'
 
 @injectable()
 export class UserController extends BaseController implements IUserController {
@@ -23,7 +24,12 @@ export class UserController extends BaseController implements IUserController {
 		super(loggerService)
 
 		this.bindRoutes([
-			{ path: '/register', method: 'post', func: this.register },
+			{
+				path: '/register',
+				method: 'post',
+				func: this.register,
+				middlewares: [new ValidateMiddleware(UserRegisterDto)],
+			},
 			{ path: '/login', method: 'post', func: this.login },
 		])
 	}
